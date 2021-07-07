9d9662d5fa2d7e603f5b85b95b6bfc2cd9fe9527
Motahharul <mdmotahharul.haque@gmail.com>
2021-07-07 23:00:22 +0530

    oauth_1

---
+++ SpringBootSecutiryOAuth-1/src/main/java/com/spring/security/SpringBootSecutiryOAuth1Application.java
@@ -0,0 +1,13 @@
+package com.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecutiryOAuth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecutiryOAuth1Application.class, args);
+	}
+
+}
